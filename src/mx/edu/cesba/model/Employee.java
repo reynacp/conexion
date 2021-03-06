2cb78f7838997aeaff9b4c5dad407596401bac62
Samsung <Samsung@DESKTOP-KGE5MCR>
2021-03-06 11:11:33 -0600

    version1.2

--- src/mx/edu/cesba/model/Employee.java
+++ src/mx/edu/cesba/model/Employee.java
@@ -7,14 +7,84 @@ package mx.edu.cesba.model;
 
 public class Employee {
     
-    public int id;
-    public String firstName;
-    public String lastName;
-    public int age;
-    public String email;
-    public int phoneNumber;
-    public double salary;
-    public String hiredate;
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String email;
+    private int phoneNumber;
+    private double salary;
+    private String hiredate;
+    public int setId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getHiredate() {
+        return hiredate;
+    }
+
+    public void setHiredate(String hiredate) {
+        this.hiredate = hiredate;
+    }
+
+    
+    
+    
+    
     
     
     public void getDetails () {
